c35e53d31b5b5f264589f9112189a20ed760f1e2
Ninnon <trentonneilson14@hotmail.com>
2018-12-26 19:16:20 -0700

    unlock HttpClient for Cards Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardsService } from './cards.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'kings';
+export class AppComponent implements OnInit {
+
+  constructor(private cardsService: CardsService) {}
+  
+  ngOnInit() {
+    this.getDeckId();
+  }
+
+  getDeckId() {
+    this.cardsService.getDeckId();
+  }
+
 }
---
+++ src/app/cards.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardsService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getDeckId() {
+    
+  }
+}
